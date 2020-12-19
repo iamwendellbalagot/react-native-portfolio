df0d19620e80a4bced4c9d8f40cee39612f788c6
iamwendellbalagot <johnwendellbalagot11111@gmail.com>
2020-12-20 05:03:10 +0800

    Added Buttons

--- Examples/Stopwatch/Stopwatch.js
+++ Examples/Stopwatch/Stopwatch.js
@@ -1,22 +1,79 @@
 import React, {useState, useEffect} from 'react';
 import { Text, View, ScrollView, TouchableOpacity } from 'react-native';
+import moment from 'moment';
 import {styles} from './styles';
 
 
 const Timer =({time}) => {
+    const timeDuration = moment.duration(time)
+    const formatTime =(n) => n < 10 ? '0'+n : n;
+    const minutes = formatTime(timeDuration.minutes());
+    const seconds = formatTime(timeDuration.seconds());
+    const miliSec = formatTime(Math.floor(timeDuration.milliseconds()/10));
     return (
         <View style={styles.timer__container}>
-            <Text style={styles.timer}>{time}</Text>
+            <Text 
+                style={styles.timer}>
+            {`${minutes}:${seconds}:${miliSec}`}
+            </Text>
+        </View>
+    );
+};
+
+const TimeToBeat = ({laps}) => {
+    const minRef = moment.duration(Math.min.apply(Math, laps))
+    const maxRef = moment.duration(Math.max.apply(Math, laps))
+    return (
+        <View style={styles.metrics__container}>
+            <Text style={styles.metrics__time}>
+                {`Minimum:  ${minRef.minutes()}:${minRef.seconds()}:${Math.floor(minRef.minutes())}`}</Text>
+            <Text style={styles.metrics__time}>
+                {`Maximum:  ${maxRef.minutes()}:${maxRef.seconds()}:${Math.floor(maxRef.minutes())}`}</Text>
+        </View>
+    );
+};
+
+const ControlsContainer = ({children}) => {
+    return (
+        <View style={styles.controls__container}>{children}</View>
+    );
+};
+
+const Button = ({type}) => {
+    let btnStyles = {};
+    switch(type){
+        case 'Start':
+            btnStyles = {cl: '#50D167', bg:'#1B361F'}
+            break;
+        case 'Stop':
+            btnStyles = {cl: '#E33935', bg:'#3C1715'}
+            break;
+        default:
+            btnStyles = {cl: '#FFFFFF', bg:'#3D3D3D'}
+            break;
+    }
+    return(
+        <View 
+        style={[styles.button__container, 
+        {backgroundColor: btnStyles.bg}]}>
+            <View style={styles.button__ring}>
+                <Text style={{color: btnStyles.cl}} >{type}</Text>
+            </View>
         </View>
     );
 };
 
 const App = () => {
-    const [time, setTime] = useState(32472);
-    
+    const [time, setTime] = useState(12321);
+    const [laps, setLaps] = useState([123123,32233,223234])
     return (
         <View style={styles.app}>
             <Timer time={time} />
+            <TimeToBeat laps = {laps}/>
+            <ControlsContainer>
+                <Button type='Laps' />
+                <Button type='Start' />
+            </ControlsContainer>
         </View>
     )
 }
--- Examples/Stopwatch/styles.js
+++ Examples/Stopwatch/styles.js
@@ -7,11 +7,51 @@ export const styles = StyleSheet.create({
         backgroundColor: 'black'
     },
     timer__container: {
-        marginTop: 100
+        marginTop: 100,
+        borderColor: 'white',
+        borderWidth: 2,
+        height: 300,
+        width: 300,
+        borderRadius: 150,
+        justifyContent: 'center',
+        alignItems: 'center'
     },
     timer: {
         color: '#ccc',
         fontWeight: '200',
-        fontSize: 70
+        fontSize: 60
+    },
+
+    metrics__container:{
+        width: '100%',
+        marginTop: 20,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    metrics__time: {
+        color: '#ccc',
+
+    },
+    controls__container: {
+        width: '100%',
+        justifyContent: 'space-between',
+        paddingHorizontal: 20,
+        flexDirection: 'row'
+    },
+    button__container: {
+        height: 80,
+        width: 80,
+        borderRadius: 40,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    button__ring: {
+        height: 74,
+        width: 74,
+        borderRadius: 37,
+        borderColor: 'black',
+        borderWidth: 2,
+        justifyContent: 'center',
+        alignItems: 'center'
     }
 });
