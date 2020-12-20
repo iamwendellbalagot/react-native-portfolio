a1aaa3c0c1d201cdb9550de126bacbe47fdbb556
iamwendellbalagot <johnwendellbalagot11111@gmail.com>
2020-12-20 12:56:22 +0800

    Home UI finished

--- Examples/Grocery/Grocery.js
+++ Examples/Grocery/Grocery.js
@@ -1,13 +1,65 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {View, Text, ScrollView, TouchableOpacity, TextInput, Button} from 'react-native';
 import {NavigationContainer, StackActions} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
+import { MaterialIcons } from '@expo/vector-icons'; 
+import moment from 'moment';
+
 import {styles} from './styles';
 
-const Home = () => {
+//[COMPONENTS]
+const Log = ({item}) => {
+    return (
+        <TouchableOpacity style={styles.log__container} activeOpacity={0.5}>
+            <View style={styles.log__top}>
+                <Text style={styles.log__title} >{item.name}</Text>
+                <Text style={styles.log__date}>{item.dateCreated}</Text>
+            </View>
+            <Text style={styles.log__total} >{`Total: ${item.total}`}</Text>
+        </TouchableOpacity>
+    );
+};
+
+const CreateIcon = () => {
+    return (
+        <TouchableOpacity style={styles.createIcon__container}>
+            <MaterialIcons name="create" size={50} color="#fcf8ec" />
+        </TouchableOpacity>
+    );
+};
+
+//[CONTAINERS]
+const Home = ({navigation}) => {
+    const [logs, setlogs] = useState([
+        {
+            id: 1,
+            dateCreated: moment().format('LL'),
+            name: 'Anniversary',
+            items: [
+                {name: 'Milk', qt: 1, price: 82},
+                {name: 'Cereals', qt: 1, price: 232},
+                {name: 'Chocolates', qt: 1, price: 478}
+            ],
+            total: 603.34
+        },
+        {
+            id: 2,
+            dateCreated: moment().format('LL'),
+            name: 'School',
+            items: [
+                {name: 'Pencils', qt: 1, price: 82},
+                {name: 'Bond Paper', qt: 1, price: 232},
+                {name: 'Notebooks', qt: 1, price: 478}
+            ],
+            total: 810.32
+        }
+    ]);
     return (
         <View style={styles.home}>
-            <Text>Home</Text>
+            {logs.map(log => (
+                <Log item={log} key={log.id} />
+            ))}
+            <CreateIcon />
         </View>
     );
 };
--- Examples/Grocery/styles.js
+++ Examples/Grocery/styles.js
@@ -4,5 +4,39 @@ export const styles = StyleSheet.create({
     home: {
         height: '100%',
         backgroundColor: '#fcf8ec'
+    },
+    log__container: {
+        width: '100%',
+        borderColor: '#79a3b1',
+        borderBottomWidth: 1,
+        paddingVertical:5,
+        paddingHorizontal: 10
+    },
+    log__top: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center'
+    },
+    log__title: {
+        fontSize: 20,
+        fontWeight: 'bold'
+    },
+    log__total: {
+        color: 'gray',
+        fontWeight: 'bold'
+    },
+    log__date: {
+        fontSize: 12
+    },
+    createIcon__container: {
+        height: 70,
+        width: 70,
+        borderRadius: 40,
+        backgroundColor: '#456268',
+        justifyContent: 'center',
+        alignItems: 'center',
+        position: 'absolute',
+        right: 20,
+        bottom: 50
     }
 });
