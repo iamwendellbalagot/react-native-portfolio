5002c0559463cc897fd0b8f7ba9a0cd61e3cd7c1
iamwendellbalagot <johnwendellbalagot11111@gmail.com>
2020-12-22 11:51:39 +0800

    Added Button and board UI

--- Examples/MemoryTrainingApp/MemApp.js
+++ Examples/MemoryTrainingApp/MemApp.js
@@ -3,10 +3,25 @@ import { View, Text } from 'react-native'
 
 import {styles} from './styles';
 
+const Board =() => {
+    return (
+        <View style={styles.board}>
+        </View>
+    );
+}
+
 const App = () => {
     return (
         <View style={styles.app}>
-            <Text style={styles.app__title}>MemApp</Text>
+            <View style={{alignItems: 'center'}}>
+                <Text style={styles.app__title}>MemApp</Text>
+                <View style={styles.startStop}>
+                    <Text style={{fontWeight:'bold',
+                        color: '#d4e2d4',
+                        fontSize: 20}}>{'Start'}</Text>
+                </View>
+            </View>
+            <Board />
         </View>
     )
 }
--- Examples/MemoryTrainingApp/styles.js
+++ Examples/MemoryTrainingApp/styles.js
@@ -4,7 +4,7 @@ export const styles = StyleSheet.create({
     app: {
         height: '100%',
         backgroundColor: '#d4e2d4',
-        alignItems: 'center'
+        alignItems: 'center',
     },
     app__title: {
         fontSize: 40,
@@ -12,5 +12,21 @@ export const styles = StyleSheet.create({
         color: '#433d3c',
         paddingVertical: 20,
         marginTop: 30
+    },
+    startStop: {
+        backgroundColor: '#df7861',
+        width: 120,
+        paddingVertical: 12,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 5
+    },
+    board: {
+        borderWidth: 2,
+        borderColor: '#433d3c',
+        height: 400,
+        width:'95%',
+        marginTop: 40,
+        borderRadius: 5
     }
 });
