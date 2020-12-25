b24fc4365fa403456227172f777d1daa3a1c318a
iamwendellbalagot <johnwendellbalagot11111@gmail.com>
2020-12-26 05:44:04 +0800

    Added user slice redux

---
+++ Examples/MessagingApp/reduxSlices/userSlice.js
@@ -0,0 +1,21 @@
+import {createSlice} from '@reduxjs/toolkit'
+
+export const userSlice = createSlice({
+    name: 'user',
+    initialState: {
+        user: null
+    },
+    reducers: {
+        setUser: (state, action) => {
+            state.user = action.payload
+        },
+        removeUser: (state, action) => {
+            state.user = null
+        }
+    }
+})
+
+export const {setUser, removeUser} = userSlice.actions
+export const getUser = (state) => state.user.user
+
+export default userSlice.reducer
--- Examples/MessagingApp/store.js
+++ Examples/MessagingApp/store.js
@@ -1,9 +1,11 @@
 import {configureStore, getDefaultMiddleware} from '@reduxjs/toolkit'
 import appReducer from './reduxSlices/appSlice'
+import userSlice from './reduxSlices/userSlice'
 
 export default configureStore({
     reducer: {
-        app: appReducer
+        app: appReducer,
+        user: userSlice
     },
     middleware: getDefaultMiddleware({
         serializableCheck: false
